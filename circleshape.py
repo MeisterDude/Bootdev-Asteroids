60805e9ce793f9e59ed66a85f02cc5106e338bbd
MeisterDude <emails.fuer.mich.hierhin@gmail.com>
2025-08-09 14:20:17 +0200

    added shooting and collisions

--- circleshape.py
+++ circleshape.py
@@ -22,3 +22,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, other):
+        # Check for collision with another CircleShape
+        if self.position.distance_to(other.position) < self.radius + other.radius:
+            return True
+        return False
--- main.py
+++ main.py
@@ -6,19 +6,19 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    updatable, drawable, asteroids = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
+    updatable, drawable, asteroids, shots = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
     Player.containers = updatable, drawable
     Asteroid.containers = updatable, drawable, asteroids
     AsteroidField.containers = updatable
+    Shot.containers = shots, updatable, drawable
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
@@ -28,11 +28,17 @@ def main():
                 return
         screen.fill((0, 0, 0))  # Fill the screen with black
         updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collision(asteroid):
+                print("Game Over!")
+                return
+            for shot in shots:
+                if shot.collision(asteroid):
+                    asteroid.kill()
+                    shot.kill()
+                    break
         for item in drawable:
             item.draw(screen)
-        #player.draw(screen)  # Draw the player
-        
-        #player.update(dt)  # Update the player
         pygame.display.flip()  # Update the display
         dt = clock.tick(60) / 1000
 
